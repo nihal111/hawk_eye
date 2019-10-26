4dbee5c38a5e5364b5de2c213d44f1d2ae9bf7fb
Nihal Singh <nihal.111@gmail.com>
2019-10-26 03:59:44 -0400

    Refactoring

--- code/warpImage.py
+++ code/warpImage.py
@@ -6,7 +6,9 @@ from perturb_functions.pan import pan
 from perturb_functions.zoom import zoom
 
 
-def getMask(points, x_width, y_width):
+def getMask(points, x_min, x_max, y_min, y_max):
+    x_width = int(x_max - x_min + 1)
+    y_width = int(y_max - y_min + 1)
     # points are in the order TL, TR, BR, BL
     mask = np.zeros((x_width, y_width))
     p = path.Path([list(cood) for cood in points])
@@ -32,33 +34,57 @@ def get_bounds(transformed_corners, footballIm, padding):
     return x_min, x_max, y_min, y_max
 
 
-def apply_perturbation(corners, transformed_corners, canvasIm,
+def apply_pan(shifted_corners, non_homo_corners, inputIm_shape, canvasIm):
+    pert_points = np.array(pan(shifted_corners, delta_theta=0.2))
+    # Create mask for perturbed trapezium
+    # mask = getMask(pert_points, x_min, x_max, y_min, y_max)
+    H_perturb = cv2.findHomography(non_homo_corners, pert_points)[0]
+    edge_map_perturb = get_edge_map(inputIm_shape, canvasIm, H_perturb)
+    return edge_map_perturb, H_perturb
+
+
+def apply_zoom(shifted_corners, non_homo_corners, inputIm_shape, canvasIm):
+    pert_points = np.array(zoom(shifted_corners, sx=0.8, sy=0.8))
+    # Create mask for perturbed trapezium
+    # mask = getMask(pert_points, x_min, x_max, y_min, y_max)
+    H_perturb = cv2.findHomography(non_homo_corners, pert_points)[0]
+    edge_map_perturb = get_edge_map(inputIm_shape, canvasIm, H_perturb)
+    return edge_map_perturb, H_perturb
+
+
+def apply_perturbation(corners, transformed_corners, canvasIm, inputIm,
                        x_min, x_max, y_min, y_max):
+    '''
+    This is the wrapper function that creates all sorts of perturbations
+    to the warped input image and finds the homographies that map it back
+    a rectangle of the input shape. In doing so, we create a dictionary
+    of edgeMap -> homography (camera view to top view)
+    '''
     # Need to shift corners to map them to the canvas with H
     shifted_corners = np.array([[corner[0] - x_min, corner[1] - y_min]
                                 for corner in transformed_corners.T])
 
-    # Get trapezium after applying pan perturbation
-    perturbation = 'ZOOM'
-    if perturbation == 'PAN':
-        pert_points = np.array(pan(shifted_corners, delta_theta=0.2))
-    elif perturbation == "ZOOM":
-        pert_points = np.array(zoom(shifted_corners, sx=0.8, sy=0.8))
-
     # Find the H for rect to perturbed trapezium
     non_homo_corners = np.array([[corner[0], corner[1]]
                                  for corner in corners.T])
-    H_perturb = cv2.findHomography(non_homo_corners, pert_points)[0]
 
-    # Create mask for perturbed trapezium
-    x_width = int(x_max - x_min + 1)
-    y_width = int(y_max - y_min + 1)
-    mask = getMask(pert_points, x_width, y_width)
+    # First pair is directly the edge map for inputIm and the homography
+    H_base = cv2.findHomography(non_homo_corners, shifted_corners)[0]
+    edge_map = get_edge_map(inputIm.shape, canvasIm, H_base)
+    plt.imshow(edge_map.astype('uint8'))
+    plt.show()
 
-    # Get edge map in input space for perturbed trapezium
-    edge_map_perturb = perturbedToRect(inputIm, canvasIm, H_perturb)
+    # Generate more pairs for different perturbations
+    # Get trapezium after applying pan perturbation
+    edge_map_zoom, H_zoom = apply_zoom(
+        shifted_corners, non_homo_corners, inputIm.shape, canvasIm)
+    plt.imshow(edge_map_zoom.astype('uint8'))
+    plt.show()
 
-    return pert_points, mask, edge_map_perturb
+    edge_map_pan, H_pan = apply_pan(
+        shifted_corners, non_homo_corners, inputIm.shape, canvasIm)
+    plt.imshow(edge_map_pan.astype('uint8'))
+    plt.show()
 
 
 def warpImageOntoCanvas(inputIm, footballIm, H, x_min, x_max, y_min, y_max):
@@ -98,8 +124,13 @@ def warpImageOntoCanvas(inputIm, footballIm, H, x_min, x_max, y_min, y_max):
     return canvasIm
 
 
-def perturbedToRect(inputIm, canvasIm, H):
-    xs, ys, a = [], [], np.zeros((inputIm.shape[1], inputIm.shape[0]))
+def get_edge_map(inputIm_shape, canvasIm, H):
+    '''
+    Input: inputIn_shape- shape of the edge map returned
+    canvasIm- top view canvas with only the football field on it
+    H- the calculated homography (camera view -> top view)
+    '''
+    xs, ys, a = [], [], np.zeros((inputIm_shape[1], inputIm_shape[0]))
     for index, _ in np.ndenumerate(a):
         xs.append(index[0]), ys.append(index[1])
     input_coords = np.vstack(
@@ -108,7 +139,7 @@ def perturbedToRect(inputIm, canvasIm, H):
     transformed[0, :] = np.divide(transformed[0, :], transformed[2, :])
     transformed[1, :] = np.divide(transformed[1, :], transformed[2, :])
 
-    edge_map_perturb = np.zeros(inputIm.shape)
+    edge_map_perturb = np.zeros(inputIm_shape)
     for k in range(0, input_coords.shape[1]):
         x_input = int(input_coords[0, k])
         y_input = int(input_coords[1, k])
@@ -137,11 +168,10 @@ def warpImage(inputIm, footballIm, H, padding):
         inputIm, footballIm, H, x_min, x_max, y_min, y_max)
 
     # Get the perturbation, mask and perturbed edge map in input space
-    pert_points, mask, edge_map_perturb = apply_perturbation(
-        corners, transformed_corners, canvasIm, x_min, x_max, y_min, y_max)
+    apply_perturbation(corners, transformed_corners, canvasIm,
+                       inputIm, x_min, x_max, y_min, y_max)
 
-    return canvasIm.astype('uint8'), pert_points, mask, \
-        edge_map_perturb.astype('uint8')
+    return canvasIm.astype('uint8')
 
 
 def cv2warp(inputIm, H):
@@ -169,18 +199,18 @@ if __name__ == '__main__':
 
     plt.imshow(footballIm)
     plt.show()
-    warpIm, pan_points, mask, edge_map_perturb = warpImage(
+    warpIm = warpImage(
         bgr, footballIm, H, padding=200)
 
-    fig = plt.figure()
-    plt.imshow(inputIm)
-    plt.show()
-    plt.imshow(warpIm)
-    for (x, y) in pan_points:
-        circle = plt.Circle((x, y), 2, color=(1, 0, 0), fill=True)
-        fig.add_subplot().add_artist(circle)
-    plt.show()
-    plt.imshow(mask)
-    plt.show()
-    plt.imshow(edge_map_perturb)
-    plt.show()
+    # fig = plt.figure()
+    # plt.imshow(inputIm)
+    # plt.show()
+    # plt.imshow(warpIm)
+    # for (x, y) in pan_points:
+    #     circle = plt.Circle((x, y), 2, color=(1, 0, 0), fill=True)
+    #     fig.add_subplot().add_artist(circle)
+    # plt.show()
+    # plt.imshow(mask)
+    # plt.show()
+    # plt.imshow(edge_map_perturb)
+    # plt.show()
