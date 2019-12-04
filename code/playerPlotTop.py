846b9a1dbaf11fd3f45cb6778182a645bc980655
Monica Gupta <monica.gupta.ms@gmail.com>
2019-12-04 17:11:58 -0500

    player detection works, saving player coords

---
+++ code/playerPlotTop.py
@@ -0,0 +1,175 @@
+import numpy as np
+import cv2
+from matplotlib import path
+import matplotlib.pyplot as plt
+
+#flip this if you want to visualize
+visualize = False
+only_score = True
+
+def get_bounds(transformed_corners, footballIm, padding):
+    x_min = min(0, min(transformed_corners[0, :]))
+    x_max = max(footballIm.shape[1], max(transformed_corners[0, :]))
+    y_min = min(0, min(transformed_corners[1, :]))
+    y_max = max(footballIm.shape[0], max(transformed_corners[1, :]))
+
+    x_min -= padding
+    y_min -= padding
+    x_max += padding
+    y_max += padding
+
+    return x_min, x_max, y_min, y_max
+
+
+def warpImageOntoCanvas(inputIm, footballIm, H, x_min, x_max, y_min, y_max, top_left):
+    x_width = int(x_max - x_min + 1)
+    y_width = int(y_max - y_min + 1)
+    canvasIm = np.zeros((y_width, x_width, 3))
+    
+    xs, ys, a = [], [], np.zeros((x_width, y_width))
+    for index, _ in np.ndenumerate(a):
+        xs.append(x_min + index[0]), ys.append(y_min + index[1])
+    canvas_coords = np.vstack(
+        (np.array(xs), np.array(ys), np.ones(len(xs))))
+    transformed = np.matmul(np.linalg.inv(H), canvas_coords)
+    xs = np.divide(transformed[0, :], transformed[2, :])
+    ys = np.divide(transformed[1, :], transformed[2, :])
+    inputIm_coords = np.transpose(np.column_stack((xs, ys)))
+
+    def inside_input(x, y):
+        return x >= 0 and x < inputIm.shape[1] and \
+            y >= 0 and y < inputIm.shape[0]
+
+    footballIm_h, footballIm_w, _ = footballIm.shape
+    for i in range(footballIm_w):
+        for j in range(footballIm_h):
+            canvasIm[j][i] = footballIm[j][i]
+
+    # Uncomment if you want to plot the warped image on the canvas
+
+    for k in range(0, canvas_coords.shape[1]):
+        x_canvas = int(canvas_coords[0, k] - x_min)
+        y_canvas = int(canvas_coords[1, k] - y_min)
+        x_input, y_input = int(inputIm_coords[0, k]), int(inputIm_coords[1, k])
+        if inside_input(x_input, y_input):
+            # Copy input image to refIm
+            canvasIm[y_canvas, x_canvas] = inputIm[y_input, x_input]
+
+    return np.array(canvasIm, dtype=int)
+
+
+def transformAndShow(file_name, H, padding, top_left):
+    bgr = cv2.imread(file_name).astype(np.uint8)
+    inputIm = bgr[..., ::-1]
+
+    # Change directory to appropriate perturbation
+    # cv2.imwrite('trainA_pan/' + str(k)  + '.jpg', inputIm)
+
+    football_field = 'football_field.jpg'
+    football = cv2.imread(football_field).astype(np.uint8)
+    footballIm = football[..., ::-1]
+
+    # plt.imshow(footballIm)
+    # plt.show()
+
+    h, w, _ = bgr.shape
+    # Find input image corners
+    corners = np.array([[0, 0, 1], [w - 1, 0, 1],
+                        [w - 1, h - 1, 1], [0, h - 1, 1]]).transpose()
+    # Find trapezium corners in the warped space (top-view)
+    transformed_corners = np.matmul(H, corners)
+    transformed_corners = np.divide(
+        transformed_corners, transformed_corners[2, :])
+    
+    if only_score:
+        return transformed_corners
+        print("shouldnt be here")
+    
+    if top_left is None:
+        x1, _, y1, _ = get_bounds(transformed_corners, footballIm, padding)
+        top_left=(-x1, -y1)
+
+    f_y, f_x, f_z = footballIm.shape
+    new_footballIm = np.zeros((int(top_left[1]) + f_y, int(top_left[0]) + f_x, f_z))
+    new_footballIm[int(top_left[1]):, int(top_left[0]):, :] = footballIm
+    
+    # plt.imshow(new_footballIm)
+    # plt.show()
+    
+    # Get bounds with football field added and padding
+    x_min, x_max, y_min, y_max = get_bounds(
+        transformed_corners, new_footballIm, padding)
+
+    # Get canvas with warped input and football field
+    canvasIm=warpImageOntoCanvas(
+        inputIm, new_footballIm, H, x_min, x_max, y_min, y_max, top_left)
+
+    plt.imshow(canvasIm)
+    if visualize:
+        plt.show()
+
+    return transformed_corners
+
+
+if __name__ == '__main__':
+
+    # For dictionary images, the homographies we have saved, transforms the
+    # camera image into the top view space where football field's top left
+    # corner is at `top_left` (xmin, ymin)
+
+    # For test images from the dataset, the homography transforms the camera
+    # image into the top view space with the top left corner of the football
+    # field is at (0, 0)
+
+    # Displaying an example of each:
+    # Flip visualize to True near imports if you want to show the outputs
+    # make only_score to False if you want to use normally
+
+    # ----- Image from dictionary -----
+    
+    file_name = 'soccer_data/train_zoom/148_85.jpg'
+    homography_file = 'soccer_data/train_zoom/H148_85.npy'
+    H = np.load(homography_file)
+    
+    with open('soccer_data/top_left/148.txt') as f:
+        content = [float(line.strip()) for line in f.readlines()]
+    top_left = (content[0], content[1])
+    
+    # print(file_name, H, 0, top_left)
+    
+    transformed_corners = transformAndShow(file_name, H, padding=0, top_left=top_left)
+    transformed_corners_dict = [[corner[0] - top_left[0], corner[1] - top_left[1]] 
+                            for corner in transformed_corners.T]
+    print("Trapezium corners from dictionary image-\n", transformed_corners_dict)
+
+    # ------ Image from Dataset ------
+
+    file_name = 'soccer_data/train_val/148.jpg'
+    homography_file = 'soccer_data/train_val/148.homographyMatrix'
+    
+    with open(homography_file) as f:
+        content = f.readlines()
+        
+    H = np.zeros((3, 3))
+    for i in range(len(content)):
+        H[i] = np.array([float(x) for x in content[i].strip().split()])
+    top_left = None
+    
+    # print(file_name, H, 0, top_left)
+    
+    transformed_corners = transformAndShow(file_name, H, padding=0, top_left=top_left)
+    transformed_corners_database = [[corner[0], corner[1]] 
+                            for corner in transformed_corners.T]
+    
+    print("\nTrapezium corners from dataset image-\n", transformed_corners_database)
+    
+    
+    from shapely.geometry import Polygon
+
+    a = Polygon([(transformed_corners_dict[0][0], transformed_corners_dict[0][1]), (transformed_corners_dict[1][0], transformed_corners_dict[1][1]), (transformed_corners_dict[2][0], transformed_corners_dict[2][1]), (transformed_corners_dict[2][0], transformed_corners_dict[2][1])])
+    
+    b = Polygon([(transformed_corners_database[0][0], transformed_corners_database[0][1]), (transformed_corners_database[1][0], transformed_corners_database[1][1]), (transformed_corners_database[2][0], transformed_corners_database[2][1]), (transformed_corners_database[2][0], transformed_corners_database[2][1])])
+    
+    IoU = a.intersection(b).area / a.union(b).area
+    
+    print("IoU: ", IoU)
--- code/player_detect.py
+++ code/player_detect.py
@@ -3,15 +3,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-cluster_centers = np.load('blue_or_red.npy')
+player_temps = []
+
 
 def frame_detect(image):
-
-
+    # vidcap = cv2.VideoCapture('cutvideo.mp4')
+    # success,image = vidcap.read()
+    idx = 0
     hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
     #green range
-    lower_green = np.array([30,100, 100])
-    upper_green = np.array([90, 255, 255])
+    lower_green = np.array([45,100, 100])
+    upper_green = np.array([75, 255, 255])
     #blue range
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])
@@ -31,24 +33,38 @@ def frame_detect(image):
     #convert to hsv to gray
     res_bgr = cv2.cvtColor(res,cv2.COLOR_HSV2BGR)
     res_gray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
+    # plt.imshow(res_gray, cmap ='gray')
+    # plt.show()
+
     #Defining a kernel to do morphological operation in threshold image to 
     #get better output.
-    kernel = np.ones((13,13),np.uint8)
-    thresh = cv2.threshold(res_gray,127,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    plt.imshow(image)
-    plt.show()
+    # kernel = np.ones((13,13),np.uint8)
+    # thresh = cv2.threshold(res_gray,127,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    # thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    # plt.imshow(thresh, cmap ='gray')
+    # plt.show()
+    # thresh = cv2.resize(thresh, (256,256))
+    # thresh = cv2.resize(thresh, (256,256))
+    #find contours in threshold image     
     plt.imshow(res_gray)
-    plt.show()
-
-    im2,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    # plt.show()
+    im2,contours,hierarchy = cv2.findContours(res_gray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     prev = 0
+    x_all = []
+    y_all = []
+    h_all = []
+    w_all = []
+    font = cv2.FONT_HERSHEY_SIMPLEX
     for c in contours:
-        x,y,w,h = cv2.boundingRect(c)
+        x,y,h,w = cv2.boundingRect(c)
+
         #Detect players
-        if(h>=1.2*(w)):
-            if( w>10 ):
+        if(w>=h):
+            if(w>12 and h>= 12):
+                # print(x,y,w,h)
+
+                idx = idx+1
                 player_img = image[y:y+h,x:x+w]
                 player_hsv = cv2.cvtColor(player_img,cv2.COLOR_BGR2HSV)
                 #If player has blue jersy
@@ -66,45 +82,120 @@ def frame_detect(image):
                 image = np.copy(image)
 
                 # if(nzCount >= 20):
-                #     #Mark blue jersy players as france
+                #     #Mark blue jersy players as 
+                # print(image.shape)
+                # cv2.putText(image, 'Player', (x-2, y-2), font, 0.8, (255,0,0), 2, cv2.LINE_AA)
+                # cv2.rectangle(image,(x,y),(x+h,y+w),(255,0,0),thickness = -1)
                 
-                # print("hello")
-                # player_crop = image[y:y+h, x:x+w]
-                # plt.imshow(player_crop)
-                # plt.show()
-                # player_crop = cv2.resize(player_crop, (30, 30))
-                # player_crop = player_crop.reshape(-1)
-                
-                # dists = np.sum(np.abs(player_crop - cluster_centers), 1)
-                # print(dists)
-                # print(np.argmin(dists))
-                
-                cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),thickness = 3)
+                x_all.append(x)
+                y_all.append(y)
+                w_all.append(w)
+                h_all.append(h)
+                # print(x, h, y, w)
+                if h > 0 and w > 0:
+                    player_crop = image[y:y+h, x:x+w]
+                    player_crop = cv2.resize(player_crop, (30, 30))
+                #     plt.imshow(player_crop)
+                #     plt.show()
+                #     # print(player_crop.shape)
+                    player_crop = player_crop.reshape(-1)
+                    player_temps.append(player_crop)
+                    # print(player_crop.shape)
+
+                    
                 # else:
-            #     pass
-            # if(nzCountred>=20):
-            #     #Mark red jersy players as belgium
-            #     cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),3)
-            # else:
+                #     pass
+                # if(nzCountred>=20):
+                #     #Mark red jersy players as belgium
+                #     cv2.putText(image, 'Belgium', (x-2, y-2), font, 0.8, (0,0,255), 2, cv2.LINE_AA)
+                #     cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),3)
+                # else:
                     # pass
+        # if((h>=1 and w>=1) and (h<=30 and w<=30)):
+        #     player_img = image[y:y+h,x:x+w]
+        
+        #     player_hsv = cv2.cvtColor(player_img,cv2.COLOR_BGR2HSV)
+        #     #white ball  detection
+        #     mask1 = cv2.inRange(player_hsv, lower_white, upper_white)
+        #     res1 = cv2.bitwise_and(player_img, player_img, mask=mask1)
+        #     res1 = cv2.cvtColor(res1,cv2.COLOR_HSV2BGR)
+        #     res1 = cv2.cvtColor(res1,cv2.COLOR_BGR2GRAY)
+        #     nzCount = cv2.countNonZero(res1)
 
 
+        #     if(nzCount >= 3):
+        #         # detect football
+        #         cv2.putText(image, 'football', (x-2, y-2), font, 0.8, (0,255,0), 2, cv2.LINE_AA)
+        #         cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
+    player_temps_arr = np.array(player_temps)
+    from sklearn.cluster import KMeans
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(player_temps)
     plt.imshow(image)
     plt.show()
+    labels = kmeans.labels_
+    x_all = np.array(x_all)
+    y_all = np.array(y_all)
+    w_all = np.array(w_all)
+    h_all = np.array(h_all)
+    coords = np.column_stack((x_all+h_all//2,y_all+w_all/2))
+    for i in range(len(labels)):
+        if(labels[i]==0):
+            cv2.rectangle(image,(x_all[i],y_all[i]),(x_all[i]+h_all[i],y_all[i]+w_all[i]),(255,0,0),thickness = -1)
+        elif(labels[i]==1):
+            cv2.rectangle(image,(x_all[i],y_all[i]),(x_all[i]+h_all[i],y_all[i]+w_all[i]),(0,0,255),thickness = -1)
+    # plt.scatter(x_all[0],y_all[0],color='red')
+    # plt.scatter(x_all[0],y_all[0]+w_all[0],color='blue')
+    # plt.scatter(x_all[0]+h_all[0],y_all[0]+w_all[0],color='yellow')
+    # plt.scatter(coords[:,0],coords[:,1],color='white')
+    # plt.scatter(x_all[0]+h_all[0],y_all[0],color='purple')
+    plt.imshow(image)
+    plt.show()
+    #         if(nzCount >= 3):
+    #             # detect football
+    #             cv2.putText(frame, 'football', (x-2, y-2), font, 0.8, (0,255,0), 2, cv2.LINE_AA)
+    #             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
 
 
+    #     cv2.imwrite("./Cropped/frame%d.jpg" % count, res)
+    #     print( 'Read a new frame: ', success  )   # save frame as JPEG file	
+    #     count += 1
+    #     cv2.imshow('Match Detection',frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+        
+    # cv2.destroyAllWindows()
+    return coords, labels
 
 if __name__ == '__main__':
     
-    for k in range(10, 13):
+    for k in range(22, 23):
     
-        file_name = '/home/rohit/Documents/soccer_data/raw/train_val/' + str(k)
+        file_name = 'soccer_data/test/' + str(k)
+        football_field = 'football_field.jpg'
 
+        # with open('{}.homographyMatrix'.format(file_name)) as f:
+        #     content = f.readlines()
+        # H = np.zeros((3, 3))
+        # for i in range(len(content)):
+        #     H[i] = np.array([float(x) for x in content[i].strip().split()])
         bgr = cv2.imread('{}.jpg'.format(file_name)).astype(np.uint8)
         inputIm = bgr[..., ::-1]
 
-        frame_detect(inputIm)
+        coords, labels = frame_detect(inputIm)
+        labels = np.array(labels)
+        np.save('soccer_data/coords/'+str(k),coords)
+        np.save('soccer_data/labels/'+str(k),labels)
 
            
+        
+        ## Change directory to appropriate perturbation
+        # cv2.imwrite('trainA_pan/' + str(k)  + '.jpg', inputIm)
+
+        # football = cv2.imread(football_field).astype(np.uint8)
+        # footballIm = football[..., ::-1]
+
+        # # plt.imshow(footballIm)
+        # # plt.show()
+        # warpIm = warpImage( bgr, footballIm, H, padding=200, idx = k)
 
         
